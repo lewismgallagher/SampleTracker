1d3cd23e9d67a113912b9eb6fad4836543e3faf4
lewis.gallagher <lewis.gallagher@axiomvetlab.co.uk>
2024-07-16 14:23:36 +0100

    Initial commit

---
+++ SampleTracker/SampleTracker/Program.cs
@@ -0,0 +1,31 @@
+using SampleTracker.Client.Pages;
+using SampleTracker.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SampleTracker.Client._Imports).Assembly);
+
+app.Run();
