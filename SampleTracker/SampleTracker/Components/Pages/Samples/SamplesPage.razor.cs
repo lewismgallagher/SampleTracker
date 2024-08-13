402a501e38ce84e1ba01b52f046489b96e584ee7
lewis.gallagher <lewis.gallagher@axiomvetlab.co.uk>
2024-08-13 12:58:06 +0100

    WIP Added sample types dropdown and some functions to samples page

--- SampleTracker/SampleTracker/Components/Pages/Samples/SamplesPage.razor.cs
+++ SampleTracker/SampleTracker/Components/Pages/Samples/SamplesPage.razor.cs
@@ -1,6 +1,7 @@
 using BAL.DTOs;
 using DAL.Data.Entities;
 using Microsoft.AspNetCore.Components;
+using System.Linq;
 
 namespace SampleTracker.Components.Pages.Samples
 {
@@ -14,10 +15,12 @@ namespace SampleTracker.Components.Pages.Samples
         public List<SampleDTO> Samples { get; set; }
         public List<SampleTypeDTO> SampleTypes { get; set; }
         public bool HasLoaded { get; set; }
+        public int SelectedSampleTypeId { get; set; }
+        public SampleTypeDTO SelectedSampleType { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            
+
             Rack = new SampleRackDTO();
             SampleTypes = new List<SampleTypeDTO>();
             Samples = new List<SampleDTO>();
@@ -26,9 +29,38 @@ namespace SampleTracker.Components.Pages.Samples
             Rack = await SampleRackService.GetRack(rackId);
             SampleTypes = await SampleRackService.GetSampleTypes();
             Samples = await SampleRackService.GetRackSamples(rackId);
-            
+            SelectedSampleType = new SampleTypeDTO();
             HasLoaded = true;
 
         }
+
+        public bool CheckSampleExistsInRack(int col, int row)
+        {
+            return Samples.Any(s => s.ColumnNumber == col && s.RowNumber == row);
+        }
+
+        public SampleDTO GetSample(int col, int row)
+        {
+            return Samples.FirstOrDefault(s => s.ColumnNumber == col && s.RowNumber == row);
+        }
+
+        public SampleDTO CreateEmptySample(int col, int row)
+        {
+            return new SampleDTO() { ColumnNumber = col, RowNumber = row };
+        }
+
+        public void ChangeSampleType()
+        {
+            SelectedSampleType = SampleTypes.FirstOrDefault(x => x.Id == SelectedSampleTypeId);
+        }
+
+        public SampleTypeDTO GetSampleTypeFromExistingSample(int sampleTypeId)
+        {
+            return SampleTypes.FirstOrDefault(x => x.Id == sampleTypeId);
+        }
+
+
     }
+
+
 }
