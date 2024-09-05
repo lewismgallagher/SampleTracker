049dcdcdad6d7a5b4ecb9adef0faeac4325f81a9
lewis.gallagher <lewis.gallagher@axiomvetlab.co.uk>
2024-09-05 19:38:14 +0100

    Added new methods for getting samples for a rack to service and changed endpoints to match

---
+++ SampleTrackerWebAPI/Samples/SampleMapping.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleTrackerWebAPI.RackConfiguration;
+using SampleTrackerWebAPI.Samples;
+namespace SampleTrackerWebAPI.Samples
+{
+    public static class SampleMapping
+    {
+        public static IEndpointRouteBuilder MapSamples(this IEndpointRouteBuilder builder)
+        {
+            var groupBuilder = builder.MapGroup("samples");
+
+
+
+
+            // getting racks, samples and sampletypes
+            groupBuilder.MapGet("/racks/search", SamplesEndpoints.GetRacks).WithName("GetRacks");
+            groupBuilder.MapGet("/racks/", SamplesEndpoints.GetRack).WithName("SamplesGetRack");
+            groupBuilder.MapGet("/", SamplesEndpoints.GetRackSamples).WithName("GetRackSamples");
+            groupBuilder.MapGet("/sampletypes", SamplesEndpoints.GetSampleTypes).WithName("GetSampleTypes");
+
+            // used for retrieving sample, sample id and checking if exists based on the samples unique indentifying value.
+            groupBuilder.MapGet("/samplebyid", SamplesEndpoints.GetSampleByIdentifyingValue).WithName("GetSampleByIdentifyingValue");
+            groupBuilder.MapGet("/sampleidbyid", SamplesEndpoints.GetSampleIdByIdentifyingValue).WithName("GetSampleIdByIdentifyingValue");
+            groupBuilder.MapGet("/checksampleexists", SamplesEndpoints.CheckSampleExists).WithName("CheckSampleExists");
+
+            // create update and delete
+            groupBuilder.MapPost("/", SamplesEndpoints.CreateSample).WithName("CreateSample");
+            groupBuilder.MapPut("/", SamplesEndpoints.UpdateSample).WithName("UpdateSample");
+            groupBuilder.MapDelete("/", SamplesEndpoints.DeleteSample).WithName("DeleteSample");
+
+
+            return builder;
+        }
+    }
+}
